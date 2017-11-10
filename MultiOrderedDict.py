f258e86cf818a64260f34445284ff105a65d7ac2
alex <alexanderhenry@gmail.com>
2017-11-11 09:30:08 +1100

    LAKSJDD

---
+++ MultiOrderedDict.py
@@ -0,0 +1,7 @@
+class MultiOrderedDict(OrderedDict):
+    def __setitem__(self, key, value):
+        if isinstance(value, list) and key in self:
+            self[key].extend(value)
+        else:
+            super(MultiOrderedDict, self).__setitem__(key, value)
+            # super().__setitem__(key, value) in Python 3
--- ui.py
+++ ui.py
@@ -1,7 +1,3 @@
-import argparse
-import ConfigParser
-from collections import OrderedDict
-import StringIO
 from MultiOrderedDict import MultiOrderedDict
 
 
@@ -22,25 +18,3 @@ ini_str = '[root]\n' + open(openmwcfg_path, 'r').read()
 ini_fp = StringIO.StringIO(ini_str)
 openmwcfg = ConfigParser.ConfigParser(dict_type = MultiOrderedDict)
 openmwcfg.readfp(ini_fp)
-
-
-
-
-import index_data
-import esm
-import schema
-import outputs
-import magic_effects
-magic_effects.process_mgefs(options)
-import spellgen
-import materials
-import item_types
-import races
-import npcs
-npcs.process_npcs(options)
-import scripts
-import enchantments
-import itemgen
-import spells
-outputs.write()
-
